4f19c8d58426d6edbb3ca75791adaf97fb3796ba
Melle Bosboom <jubiman@users.noreply.github.com>
2020-05-04 08:57:00 +0200

    oops

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-const config = require('./config.json')
+//const config = require('./config.json')
 const bot = new Discord.Client()
 var prefix = process.env.prefix
 //var prefix = config.prefix
